e5956a34b35ed3b8e74fbc1f250b9a0fc50acbca
Pablo <parinconj@gmail.com>
2022-07-03 17:06:36 -0500

    hola

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+/**
+ *
+ * @author parin
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Hola");
+
+    }
+
+}
